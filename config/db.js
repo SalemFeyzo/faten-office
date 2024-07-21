2fefc84bb909a1a3d89803af6b57b040b64d149a
SalemFeyzo <f1989salim@gmail.com>
2024-07-21 15:01:56 +0300

    server updated

--- config/db.js
+++ config/db.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true,
     })
     console.log(`mongodb connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
